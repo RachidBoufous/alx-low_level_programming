c3bc3a37185397c65281bda949361f3483b86add
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-21 21:24:29 +0100

    task 5: sign

--- 0x02-functions_nested_loops/main/1-main.c
+++ 0x02-functions_nested_loops/main/1-main.c
@@ -10,6 +10,7 @@ int main(void)
     /*print_alphabet();*/
     /*print_alphabet_x10();*/
     /*_islower('a');*/
-    _isalpha('-');
+    /*_isalpha('a');*/
+    print_sign(98);
     return (0);
 }
