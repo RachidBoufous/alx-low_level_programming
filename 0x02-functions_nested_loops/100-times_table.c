e48b1eaca0c3f1445f2e17dde656a8d569d9992c
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-22 00:16:28 +0100

    fix task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,9 +5,9 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			if (i * j < 10)
 			{
