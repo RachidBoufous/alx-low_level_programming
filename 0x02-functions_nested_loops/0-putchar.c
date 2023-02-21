5e8a28420a0931555a7b4a4cf3f3622765237cb1
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-21 16:10:51 +0100

    task1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+* main - entry point
+* description: prints _putchar
+* Return: Always 0.
+*/
+int main(void)
+{
+char name[] = "_putchar";
+int i = 0;
+for (i = 0; i < 8; i++)
+{
+_putchar(name[i]);
+}
+return (0);
+
+}
