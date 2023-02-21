77adb83583604de1955a178a44f1797126f50e5e
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-21 23:53:14 +0100

    task 11 last fix plz work

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,6 +25,20 @@ void print_to_98(int n)
 			}
 		}
 	}
+	else if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
 	else if (n == 98)
 	{
 		printf("%d", n);
