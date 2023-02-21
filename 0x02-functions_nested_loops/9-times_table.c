2b030db6f699732f443316a80f253da4dd574baa
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-21 23:35:53 +0100

    fix betty style on task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,31 @@
  * Return: void
  */
 
-void times_table(void){
-	int i,j;
-	for (i = 0; i < 10; i++){
-		for (j = 0; j < 10; j++){
-			
-			if (i * j < 10){
-				if (j != 0){
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i * j < 10)
+			{
+				if (j != 0)
+				{
 					_putchar(' ');
 					_putchar(' ');
 				}
 				_putchar(i * j + '0');
 			}
-			else{
+			else
+			{
 				_putchar(' ');
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
 			}
-			if (j != 9){
+			if (j != 9)
+			{
 				_putchar(',');
 			}
 		}
