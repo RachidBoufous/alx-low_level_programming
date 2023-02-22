bb87a65a982d90aaf316e39a29f6a73da59b576a
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-22 01:34:35 +0100

    fix fibo task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
+/**
 * main - entry point
-* Description: Prints the first 50 numbers in the Fibonacci sequence
+* description: prints the first 50 numbers in the Fibonacci sequence
 * Return: 0
 */
 
 
-int main() {
-	int count, fib1 = 0, fib2 = 1, sum;
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	/*Prints the first 50 numbers in the Fibonacci sequence*/
 	for (count = 0; count < 50; ++count)
 	{
 		sum = fib1 + fib2;
-		printf("%d", sum);
+		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
 
-		printf(", ");
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
-   return 0;
+	return (0);
 }
