caecb97e4da6f202a13645e3903de99b646f33bd
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-22 01:28:54 +0100

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+* main - entry point
+* Description: Prints the first 50 numbers in the Fibonacci sequence
+* Return: 0
+*/
+
+
+int main() {
+    int count, fib1 = 0, fib2 = 1, sum;
+
+	//Prints the first 50 numbers in the Fibonacci sequence
+	for (count = 0; count < 50; ++count)
+	{
+		sum = fib1 + fib2;
+		printf("%d", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		printf(", ");
+	}
+   return 0;
+}
