00127f89d8eb49a9b7df80a170d9373c43e4ad99
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-23 22:42:54 +0100

    fix task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,65 +1,39 @@
 #include <stdio.h>
 
 /**
- * largest_prime_factor - finds the largest prime factor of a number
- * @n: number to find the largest prime factor of
- * description: finds the largest prime factor of a number
- * Return: void
- */
-
-/* _sqrt: function to calculate square root of number*/
-double _sqrt(double x)
-{
-	float sqrt, tmp;
-
-	sqrt = x / 2;
-	tmp = 0;
-
-	while (sqrt != tmp)
-	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-	return (sqrt);
-}
-
-/* function to find the largest prime factor of a number n given*/
-
-void largest_prime_factor(long int num)
-{
-	int prmNu, largest;
-
-	/* first divide with the smallest prime number (two) */
-	while (num % 2 == 0)
-		num = num / 2;
-
-	/* num must be odd so we proceed to the next prime number (plus two) */
-	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-	{
-		while (num % prmNu == 0)
-		{
-			num = num / prmNu;
-			largest = prmNu;
-		}
-	}
-
-
-	if (num > 2)
-		largest = num;
-	printf("%d\n", largest);
-}
-
-
-/**
- * main - finds the largest prime factor of a number
- * description: finds the largest prime factor of a number
+ * main - prints the largest prime factor of the number 612852475143
+ * Description: prints the largest prime factor of the number 612852475143
  * Return: 0
  */
 
 int main(void)
 {
-	long n = 600851475143;
+	long int n;
+	long int max;
+	long int i;
 
-	largest_prime_factor(n);
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
 	return (0);
 }
