3f0dbed0431cd08db64148e9725b5a6ec571f275
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-27 17:50:06 +0100

    task 101

---
+++ 0x05-pointers_arrays_strings/101-crackme.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ * description: generates random valid passwords for the program 101-crackme
+ * Return: 0
+ * author: @RachidBoufous
+ */
+
+int main(void)
+{
+	srand((unsigned int)(time(NULL)));
+
+	int index = 0;
+	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
+	for(index = 0; index < 12; index++)
+	{
+		printf("%c", characters[rand() % (sizeof characters - 1)]);
+	}
+}
