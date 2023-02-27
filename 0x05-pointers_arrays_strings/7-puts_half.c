36c284657e69d126a54219d5351c044b532073a9
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-27 17:28:56 +0100

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,32 +5,23 @@
  * @str: string to print
  * description: prints half of a string if odd, print (length - 1) / 2
  * Return: void
- * author: @RachidBoufous
  */
 
 void puts_half(char *str)
 {
 	int i, str_len;
-	int startNum;
 
-	while (str[i] != '\0')
-	{
-		str_len++;
-		i++;
-	}
-
+	for (str_len = 0; str[str_len] != '\0'; str_len++)
+		;
 
 	if (str_len % 2 == 0)
 	{
-		startNum = str_len / 2;
+		for (i = str_len / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
 	}
 	else
 	{
-		startNum = (str_len - 1) / 2;
-	}
-
-	for (i = startNum; i < str_len; i++)
-	{
-		_putchar(str[i]);
+		for (i = ((str_len - 1) / 2) + 1 ;  str[i] != '\0'; i++)
+			_putchar(str[i]);
 	}
 }
