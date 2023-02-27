b96dc4c5d9836a7b1cfe6bee92dda75e36aa3c35
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-27 18:01:34 +0100

    fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,14 +8,12 @@
  * Return: 0
  * author: @RachidBoufous
  */
-
 int main(void)
 {
-	srand((unsigned int)(time(NULL)));
+	srand(time(0));
 
-	int index;
-	index = 0;
-	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
-	for(index = 0; index < 12; index++)
-		printf("%c", characters[rand() % (sizeof characters - 1)]);
+    printf("%d", rand());
+
+	return 0;
 }
+
