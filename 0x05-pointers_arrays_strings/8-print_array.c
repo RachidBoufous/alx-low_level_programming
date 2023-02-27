21f35afe0ced60d6563399cf510878ae340ed1cb
Rachid Boufous <rachidboufous32@gmail.com>
2023-02-27 17:32:04 +0100

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array to print
+ * @n: number of elements to print
+ * description: prints n elements of an array of integers
+ * Return: void
+ * author: @RachidBoufous
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
