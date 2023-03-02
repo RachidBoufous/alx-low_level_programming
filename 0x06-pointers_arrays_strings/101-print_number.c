def596a5de6b6a5d82d64c7a9703e1d5fc35768d
Rachid Boufous <rachidboufous32@gmail.com>
2023-03-03 00:07:08 +0100

    fixing style

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
-  *
-  * Return: Nothing
-  * On error, nothing
-  * On success, nothing
-  */
+ * print_number - Prints any integer with putchar
+ * @n: Number to prints
+ *
+ * Return: Nothing
+ * On error, nothing
+ * On success, nothing
+ */
 void print_number(int n)
 {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n/10) {
-        print_number(n/10);
-    }
-    _putchar(n%10 + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
