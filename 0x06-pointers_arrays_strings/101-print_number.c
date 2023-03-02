b7eae63acfad1e9260bab8f5d5b8f5eea840fdae
Rachid Boufous <rachidboufous32@gmail.com>
2023-03-03 00:11:25 +0100

    fixing 101 and 102

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - Prints any integer with putchar
@@ -8,16 +9,43 @@
  * On error, nothing
  * On success, nothing
  */
+
 void print_number(int n)
 {
+	int power;
+	int neg;
+	int hold;
+
+	neg = 0;
+	power = 1;
+	hold = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		neg = 1;
 	}
-	if (n / 10)
+	while (hold > 9 || hold < -9)
 	{
-		print_number(n / 10);
+		power *= 10;
+		hold /= 10;
 	}
-	_putchar(n % 10 + '0');
+	while (power > 0)
+	{
+		if (power > 9)
+		{
+			if (!neg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (neg)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
+	}
 }
