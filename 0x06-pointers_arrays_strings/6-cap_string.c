b69b36a2b7aa94a6d1737b1d1df9e226ecc1319b
Rachid Boufous <rachidboufous32@gmail.com>
2023-03-02 22:30:11 +0100

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,38 @@
  */
 
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	/*iterate through our array values*/
+	while (s[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		|| str[i] == ',' || str[i] == ';' || str[i] == '.'
-		|| str[i] == '!' || str[i] == '?' || str[i] == '"'
-		|| str[i] == '(' || str[i] == ')' || str[i] == '{'
-		|| str[i] == '}')
+		/*check for any lowercase letters*/
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
+			if (i == 0)
 			{
-				str[i + 1] = str[i + 1] - 32;
+				s[i] -= 32;
+			}
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
 			}
 		}
+		i++;
 	}
-	return (str);
+	return (s);
 }
