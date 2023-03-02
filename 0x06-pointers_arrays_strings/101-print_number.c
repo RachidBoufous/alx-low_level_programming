cd5f7a8564983400fec2ba0047a96de13d9fd027
Rachid Boufous <rachidboufous32@gmail.com>
2023-03-02 23:36:14 +0100

    fixing tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,18 @@
 
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int num;
 
-	if (n < 0)
+	num = n;
+	if (n <  0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -n;
 	}
 
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
 }
