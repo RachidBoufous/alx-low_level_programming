3e016c472a262259f26a3a237b372a260d6dbed2
Rachid Boufous <rachidboufous32@gmail.com>
2023-03-06 21:37:30 +0100

    fix betty style

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * _strspn - gets the length of a prefix substring
+ * description: gets the length of a prefix substring
+ * @s: pointer to string
+ * @accept: pointer to substring
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, k;
