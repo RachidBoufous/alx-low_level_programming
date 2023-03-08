a033712e77c92127eb605e29b492029a33af31f9
Rachid Boufous <rachidboufous32@gmail.com>
2023-03-08 21:08:04 +0100

    optional tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,36 @@
 #include "main.h"
+
+int actual_is_palindrome(char *s, int len);
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: string to be checked
+ *
+ * Return: 1 if s is a palindrome, otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	if (len <= 1)
+		return (1);
+	return (actual_is_palindrome(s, len));
+}
+
+/**
+ * actual_is_palindrome - recurses to check if a string is a palindrome
+ * @s: string to be checked
+ * @len: length of string
+ * Description: Checks if a string is a palindrome.
+ * Return: 1 if s is a palindrome, otherwise 0
+ */
+
+int actual_is_palindrome(char *s, int len)
+{
+	if (*s != *(s + len - 1))
+		return (0);
+	if (len <= 3)
+		return (1);
+	return (actual_is_palindrome(s + 1, len - 2));
+}
