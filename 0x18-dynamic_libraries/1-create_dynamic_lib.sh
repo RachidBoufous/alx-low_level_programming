038e86ffe54de51b23459fa49fefde5fe801467a
RachidBoufous <rachidboufous32gmail.com>
2023-05-23 00:37:16 +0000

    trying to fix task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+
+# Create a list of all the .c files in the current directory
+cfiles=( *.c )
+
+# Compile all the .c files into object files
+for cfile in "${cfiles[@]}"; do
+  gcc -c -fPIC "$cfile"
+done
+
+# Link all the object files together to create a dynamic library
+gcc -shared -o liball.so "${cfiles[@]}"
